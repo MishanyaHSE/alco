d3a168b1f735f17749643bd3a4edd7a2cf510ad7
mishanya <mmlobanovich_1@edu.hse.ru>
2023-08-08 15:07:15 +0300

    Added brain scene with animation

--- src/main/java/michael/alco/controllers/BrainController.java
+++ src/main/java/michael/alco/controllers/BrainController.java
@@ -6,11 +6,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
+import michael.alco.model.DiseasesBase;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -19,42 +21,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BrainController implements Initializable {
+
     @FXML
     ImageView imageView;
+
     @FXML
     Circle circle;
+
+    @FXML
+    Label brainLabel;
 
     private boolean isBlack = true;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            Image image = new Image(new FileInputStream("src/main/java/michael/alco/controllers/splash.jpg"));
-            imageView.setImage(image);
-            imageView.setFitHeight(800);
-            imageView.setFitWidth(600);
-            imageView.setPreserveRatio(true);
-            circle.setRadius(50);
-            circle.setFill(Color.RED);
-            circle.setOpacity(0.3);
-            circle.setStrokeWidth(2);
-            Timeline tl = new Timeline(new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    if (isBlack) {
-                        circle.setStroke(Color.WHITE);
-                        isBlack = false;
-                    } else {
-                        circle.setStroke(Color.BLACK);
-                        isBlack = true;
-                    }
-                }
-            }));
-            tl.setCycleCount(Timeline.INDEFINITE);
-            tl.play();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+            addBrainImage();
+            addCircleWithAnimation();
+            addTextToLabel();
     }
 
     @FXML
@@ -70,5 +53,48 @@ public class BrainController implements Initializable {
     @FXML
     private void nextClick() {
 
+    }
+
+    private void addBrainImage() {
+        try {
+            Image image = new Image(new FileInputStream("src/main/java/michael/alco/controllers/brain.png"));
+            imageView.setImage(image);
+            imageView.setFitHeight(440);
+            imageView.setFitWidth(500);
+            imageView.setPreserveRatio(true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addCircleWithAnimation() {
+        if (DiseasesBase.percent == 25) {
+            circle.setRadius(50);
+        } else if (DiseasesBase.percent == 35) {
+            circle.setRadius(70);
+        }
+        circle.setFill(Color.RED);
+        circle.setOpacity(0.6);
+        circle.setStrokeWidth(2);
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(100), actionEvent -> {
+            if (isBlack) {
+                circle.setStroke(Color.WHITE);
+                isBlack = false;
+            } else {
+                circle.setStroke(Color.BLACK);
+                isBlack = true;
+            }
+        }));
+        tl.setCycleCount(Timeline.INDEFINITE);
+        tl.play();
+    }
+
+    private void addTextToLabel() {
+        if (DiseasesBase.percent <= 35) {
+            brainLabel.setText("Красным отмечены зоны поражения. Поражено менее " + DiseasesBase.percent +
+                    "%, гарантия эффективности лечения более 98%.");
+        } else {
+            brainLabel.setText("Bad case");
+        }
     }
 }
